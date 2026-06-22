01cc0993c2e0713a0303545dbb525ba3080e2bd7
Eslem Nur <eslemnur_notes@proton.me>
2026-06-22 22:59:11 +0200

    Create sw2.js

---
+++ sw2.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'eslem-nur-v2';
+const urlsToCache = [
+  
+  '/eslemnur_notes/ikuran-siyah.html',
+  '/eslemnur_notes/favicon2.ico',
+  '/eslemnur_notes/favicon2-96x96.png',
+  '/eslemnur_notes/site2.webmanifest',
+  // İstersen diğer CSS/JS dosyalarını da buraya ekleyebilirsin
+];
+
+// Service Worker'ı kaydet
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Önbelleğe alındı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Eski sürümleri sil
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// İstekleri yakala ve önbellekten döndür
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
