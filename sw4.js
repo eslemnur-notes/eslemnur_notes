79b1c437da80d83f0c7641baf55467fac4443ab2
Eslem Nur <eslemnur_notes@proton.me>
2026-06-23 04:16:38 +0200

    Create sw4.js

---
+++ sw4.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'eslem-nur-v4';
+const urlsToCache = [
+  'eslemnur_notes/arapca.html',
+  '/eslemnur_notes/gelisim.html',
+  '/eslemnur_notes/umre-hac.html',
+  '/eslemnur_notes/favicon4.ico',
+  '/eslemnur_notes/favicon4-96x96.png',
+  '/eslemnur_notes/site4.webmanifest',
+  // İstersen diğer CSS/JS dosyalarını da buraya ekleyebilirsin
+];
+
+// Service Worker'ı kaydet
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Önbelleğe alındı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Eski sürümleri sil
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// İstekleri yakala ve önbellekten döndür
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
